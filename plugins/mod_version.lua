e33ecf3bff2a419a024d773ba50dcd3185592656
Marco Cirillo <maranda@lightwitch.org>
2012-10-03 14:42:43 +0000

    mod_version: adding random OS version command switcher.

--- plugins/mod_version.lua
+++ plugins/mod_version.lua
@@ -8,11 +8,13 @@ local query = st.stanza("query", {xmlns = "jabber:iq:version"})
 	:tag("name"):text("Metronome"):up()
 	:tag("version"):text(metronome.version):up();
 
-if not module:get_option("hide_os_type") then
+local random_osv_cmd = module:get_option_string("refresh_random_osv_cmd");
+
+if not module:get_option_boolean("hide_os_type") and not random_osv_cmd then
 	if os.getenv("WINDIR") then
 		version = "Windows";
 	else
-		local os_version_command = module:get_option("os_version_command");
+		local os_version_command = module:get_option_string("os_version_command");
 		local ok, pposix = pcall(require, "util.pposix");
 		if not os_version_command and (ok and pposix and pposix.uname) then
 			version = pposix.uname().sysname;
@@ -34,7 +36,14 @@ end
 module:hook("iq/host/jabber:iq:version:query", function(event)
 	local stanza = event.stanza;
 	if stanza.attr.type == "get" and stanza.attr.to == module.host then
-		event.origin.send(st.reply(stanza):add_child(query));
+		local _query;
+		if random_osv_cmd then
+			random_string = io.popen(random_osv_cmd);
+			version = random_string and random_string:read("*a"); version = version and version:match("^%s*(.-)%s*$") or version;
+			random_string:close();
+			_query = st.clone(query):tag("os"):text(version):up();
+		end
+		event.origin.send(st.reply(stanza):add_child(_query or query));
 		return true;
 	end
 end);
