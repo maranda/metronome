21769cbbb36137d2a92a728827199c7295332c33
Marco Cirillo <maranda@lightwitch.org>
2013-02-26 20:41:49 +0100

    muc.lib.lua: optimize code a bit.

--- plugins/muc/muc.lib.lua
+++ plugins/muc/muc.lib.lua
@@ -1058,7 +1058,7 @@ function room_mt:set_affiliation(actor, jid, affiliation, callback, reason, dumm
 		end
 		if actor_affiliation ~= "owner" then
 			if actor_affiliation ~= "admin" or target_affiliation == "owner" or target_affiliation == "admin" or
-			   (actor_affiliation == "admin" and (not dummy and (affiliation == "owner" or affiliation == "admin"))) then
+			   (not dummy and (affiliation == "owner" or affiliation == "admin")) then
 				return nil, "cancel", "not-allowed";
 			end
 		elseif target_affiliation == "owner" and jid_bare(actor) == jid then -- self change
