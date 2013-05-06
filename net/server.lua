69b523f10c3f0c8c33313de0cafb35a9b4ff3cd7
Marco Cirillo <maranda@lightwitch.org>
2013-05-06 19:30:29 +0200

    net.server, util.timer: updating to reflect changes to the server backend.

--- net/server.lua
+++ net/server.lua
@@ -1,4 +1,50 @@
--- This shall be keept for eventual future support of alternative server backends
--- e.g. Libevent
+local server;
 
-local server = require "net.server_select" ; return server
+server = require "net.server_event";
+
+local ok, signal = pcall(require, "util.signal");
+if ok and signal then
+	local _signal_signal = signal.signal;
+	function signal.signal(signal_id, handler)
+		if type(signal_id) == "string" then
+			signal_id = signal[signal_id:upper()];
+		end
+		if type(signal_id) ~= "number" then
+			return false, "invalid-signal";
+		end
+		return server.hook_signal(signal_id, handler);
+	end
+end
+
+if metronome then
+	local config_get = require "core.configmanager".get;
+	local defaults = {};
+	for k,v in pairs(server.cfg or server.getsettings()) do
+		defaults[k] = v;
+	end
+	local function load_config()
+		local settings = config_get("*", "network_settings") or {};
+		local event_settings = {
+			ACCEPT_DELAY = settings.event_accept_retry_interval;
+			ACCEPT_QUEUE = settings.tcp_backlog;
+			CLEAR_DELAY = settings.event_clear_interval;
+			CONNECT_TIMEOUT = settings.connect_timeout;
+			DEBUG = settings.debug;
+			HANDSHAKE_TIMEOUT = settings.ssl_handshake_timeout;
+			MAX_CONNECTIONS = settings.max_connections;
+			MAX_HANDSHAKE_ATTEMPTS = settings.max_ssl_handshake_roundtrips;
+			MAX_READ_LENGTH = settings.max_receive_buffer_size;
+			MAX_SEND_LENGTH = settings.max_send_buffer_size;
+			READ_TIMEOUT = settings.read_timeout;
+			WRITE_TIMEOUT = settings.send_timeout;
+		};
+
+		for k,default in pairs(defaults) do
+			server.cfg[k] = event_settings[k] or default;
+		end
+	end
+	load_config();
+	metronome.events.add_handler("config-reloaded", load_config);
+end
+
+return server;
