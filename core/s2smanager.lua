37df890a5339307a7592b3bd186af8b541baeae1
Marco Cirillo <maranda@lightwitch.org>
2013-05-08 22:19:53 +0200

    s2smanager: do not clean from_host and to_host from resting sessions.

--- core/s2smanager.lua
+++ core/s2smanager.lua
@@ -122,7 +122,7 @@ local resting_session = { -- Resting, not dead
 function retire_session(session, reason)
 	local log = session.log or log;
 	for k in pairs(session) do
-		if k ~= "trace" and k ~= "log" and k ~= "id" and k ~= "conn" then
+		if k ~= "trace" and k ~= "log" and k ~= "id" and k ~= "conn" and k ~= "from_host" and k ~= "to_host" then
 			session[k] = nil;
 		end
 	end
