90ae789b3cc05d21b1e9922c3b2b7c070b724caf
Marco Cirillo <maranda@lightwitch.org>
2013-05-15 18:45:15 +0200

    mod_messagefilter: cleanup and simplify code.

--- plugins/mod_messagefilter.lua
+++ plugins/mod_messagefilter.lua
@@ -5,51 +5,38 @@
 -- information.
 
 local st = require "util.stanza";
-local jid = require "util.jid";
+local split = require "util.jid".split;
 
 local patterns = config.get(module:get_host(), "core", "messagefilter_patterns") or {};
 local hosts = config.get(module:get_host(), "core", "messagefilter_chosts") or {};
 
 local bounce_message = config.get(module:get_host(), "core", "messagefilter_bmsg") or "Message rejected by server filter";
 
-local function message_filter(data)
-	local origin, stanza = data.origin, data.stanza;
-	local body = stanza:child_with_name("body");
-	local fromnode, fromhost = jid.split(stanza.attr.from);
+local function message_filter(event)
+	local origin, stanza = event.origin, event.stanza;
+	local body_text = stanza:child_with_name("body") and stanza:child_with_name("body"):get_text();
+	local fromnode, fromhost = split(stanza.attr.from);
 
 	local error_reply = st.message{ type = "error", from = stanza.attr.to }
 					:tag("error", {type = "modify"})
 						:tag("not-acceptable", {xmlns = "urn:ietf:params:xml:ns:xmpp-stanzas"})
 							:tag("text", {xmlns = "urn:ietf:params:xml:ns:xmpp-stanzas"}):text(bounce_message):up();
 
-	if body then
-		if #body.tags ~= 0 then
-			origin.send(st.error_reply(stanza, "modify", "not-acceptable", "Your client sent an invalid message"));
-			return true;
-		end
-
-		local test_host = false;
-		for _, host in ipairs(hosts) do
-			if fromhost == host then test_host = true; break; end
-		end
+	if body_text then
+		local host = hosts[fromhost];
+		if host and not host.modules.auth_anonymous then return; end
 		
 		for _, pattern in ipairs(patterns) do
-			if test_host then
-				if body[1]:match(pattern) then
-					error_reply.attr.to = stanza.attr.from;
-					origin.send(error_reply);
-					module:log("info", "Bounced message from anon user %s because it contained profanity", stanza.attr.from);
-					return true; -- Drop the stanza now
-				end
+			if body_text:match(pattern) then
+				error_reply.attr.to = stanza.attr.from;
+				origin.send(error_reply);
+				module:log("info", "Bounced message from anon user %s because it contained profanity", stanza.attr.from);
+				return true; -- Drop the stanza now
 			end
 		end
 	end
 end
 
-
-function module.load()
-	module:hook("message/bare", message_filter, 500);
-	module:hook("message/full", message_filter, 500);
-	module:hook("message/host", message_filter, 500);
-end
-
+module:hook("message/bare", message_filter, 500);
+module:hook("message/full", message_filter, 500);
+module:hook("message/host", message_filter, 500);
