155cec82adc515528bfc26859ac27a91d1892c59
Marco Cirillo <maranda@lightwitch.org>
2013-05-24 15:27:27 +0200

    mod_stanza_counter: making this a global shared module, bundling together.

--- plugins/mod_stanza_counter.lua
+++ plugins/mod_stanza_counter.lua
@@ -4,20 +4,19 @@
 -- ISC License, please see the LICENSE file in this source package for more
 -- information about copyright and licensing.
 
+module:set_global()
+
 local jid_bare = require "util.jid".bare
 
 -- Setup, Init functions.
 -- initialize function counter table on the global object on start
-local function init_counter()
+function init_counter()
 	metronome.stanza_counter = { 
 		iq = { incoming=0, outgoing=0 },
 		message = { incoming=0, outgoing=0 },
 		presence = { incoming=0, outgoing=0 }
 	}
 end
-
--- Setup on server start
-local function setup() init_counter() end
 
 -- Basic Stanzas' Counters
 local function iq_callback(check)
@@ -65,31 +64,33 @@ local function pre_callback(check)
 	end
 end
 
--- Hook all pre-stanza events.
-module:hook("pre-iq/bare", iq_callback(true), 140)
-module:hook("pre-iq/full", iq_callback(true), 140)
-module:hook("pre-iq/host", iq_callback(true), 140)
+function module.add_host(module)
+	-- Hook all pre-stanza events.
+	module:hook("pre-iq/bare", iq_callback(true), 140)
+	module:hook("pre-iq/full", iq_callback(true), 140)
+	module:hook("pre-iq/host", iq_callback(true), 140)
 
-module:hook("pre-message/bare", mes_callback(true), 140)
-module:hook("pre-message/full", mes_callback(true), 140)
-module:hook("pre-message/host", mes_callback(true), 140)
+	module:hook("pre-message/bare", mes_callback(true), 140)
+	module:hook("pre-message/full", mes_callback(true), 140)
+	module:hook("pre-message/host", mes_callback(true), 140)
 
-module:hook("pre-presence/bare", pre_callback(true), 140)
-module:hook("pre-presence/full", pre_callback(true), 140)
-module:hook("pre-presence/host", pre_callback(true), 140)
+	module:hook("pre-presence/bare", pre_callback(true), 140)
+	module:hook("pre-presence/full", pre_callback(true), 140)
+	module:hook("pre-presence/host", pre_callback(true), 140)
 
--- Hook all stanza events.
-module:hook("iq/bare", iq_callback(false), 140)
-module:hook("iq/full", iq_callback(false), 140)
-module:hook("iq/host", iq_callback(false), 140)
+	-- Hook all stanza events.
+	module:hook("iq/bare", iq_callback(false), 140)
+	module:hook("iq/full", iq_callback(false), 140)
+	module:hook("iq/host", iq_callback(false), 140)
 
-module:hook("message/bare", mes_callback(false), 140)
-module:hook("message/full", mes_callback(false), 140)
-module:hook("message/host", mes_callback(false), 140)
+	module:hook("message/bare", mes_callback(false), 140)
+	module:hook("message/full", mes_callback(false), 140)
+	module:hook("message/host", mes_callback(false), 140)
 
-module:hook("presence/bare", pre_callback(false), 140)
-module:hook("presence/full", pre_callback(false), 140)
-module:hook("presence/host", pre_callback(false), 140)
+	module:hook("presence/bare", pre_callback(false), 140)
+	module:hook("presence/full", pre_callback(false), 140)
+	module:hook("presence/host", pre_callback(false), 140)
+end
 
--- Hook server start to initialize the counter.
-module:hook("server-started", setup)
+-- Set up!
+module:hook("server-started", init_counter);
