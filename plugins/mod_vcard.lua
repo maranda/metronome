4510ad6619ed07a728e0884d4eaa3214c2ced483
Marco Cirillo <maranda@lightwitch.org>
2013-06-09 16:01:56 +0200

    mod_vcard: mild code refactor, - have the module fire a global vcard-updated event each time a vcard is updated for other modules to listen to; - also have it listen to a global vcard-synchronize event and process events related to its host, which returns true and the vcard if successful or false and nil if not.

--- plugins/mod_vcard.lua
+++ plugins/mod_vcard.lua
@@ -7,11 +7,25 @@
 -- As per the sublicensing clause, this file is also MIT/X11 Licensed.
 -- ** Copyright (c) 2008-2012, Matthew Wild, Waqas Hussain
 
-local st = require "util.stanza"
+local st = require "util.stanza";
 local jid_split = require "util.jid".split;
-local datamanager = require "util.datamanager"
+local datamanager = require "util.datamanager";
+local metronome = metronome;
 
 module:add_feature("vcard-temp");
+
+local function handle_synchronize(event)
+	local node, host = event.node, event.host;
+	if host ~= module.host then return; end
+
+	local vCard = st.deserialize(datamanager.load(node, host, "vcard"));
+
+	if vCard then
+		return true, vCard;
+	else
+		return false, nil;
+	end
+end		
 
 local function handle_vcard(event)
 	local session, stanza = event.origin, event.stanza;
@@ -31,8 +45,11 @@ local function handle_vcard(event)
 		end
 	else
 		if not to then
-			if datamanager.store(session.username, session.host, "vcard", st.preserialize(stanza.tags[1])) then
+			local vCard = st.preserialize(stanza.tags[1]);
+			
+			if datamanager.store(session.username, session.host, "vcard", vCard) then
 				session.send(st.reply(stanza));
+				metronome.events.fire_event("vcard-updated", { node = session.username, host = session.host, vcard = vCard });
 			else
 				-- TODO unable to write file, file may be locked, etc, what's the correct error?
 				session.send(st.error_reply(stanza, "wait", "internal-server-error"));
@@ -44,5 +61,6 @@ local function handle_vcard(event)
 	return true;
 end
 
+module:hook_global("vcard-synchronize", handle_synchronize);
 module:hook("iq/bare/vcard-temp:vCard", handle_vcard);
 module:hook("iq/host/vcard-temp:vCard", handle_vcard);
