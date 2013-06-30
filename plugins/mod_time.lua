647ea27712ee85ca83586f9815ac4e72257c1e25
Marco Cirillo <maranda@lightwitch.org>
2013-06-30 21:55:56 +0200

    mod_time: simplify a bit, XEP-090 is deprecated so support only XEP-202.

--- plugins/mod_time.lua
+++ plugins/mod_time.lua
@@ -9,7 +9,6 @@
 
 local st = require "util.stanza";
 local datetime = require "util.datetime".datetime;
-local legacy = require "util.datetime".legacy;
 
 -- XEP-0202: Entity Time
 
@@ -27,19 +26,3 @@ end
 
 module:hook("iq/bare/urn:xmpp:time:time", time_handler);
 module:hook("iq/host/urn:xmpp:time:time", time_handler);
-
--- XEP-0090: Entity Time (deprecated)
-
-module:add_feature("jabber:iq:time");
-
-local function legacy_time_handler(event)
-	local origin, stanza = event.origin, event.stanza;
-	if stanza.attr.type == "get" then
-		origin.send(st.reply(stanza):tag("query", {xmlns = "jabber:iq:time"})
-			:tag("utc"):text(legacy()));
-		return true;
-	end
-end
-
-module:hook("iq/bare/jabber:iq:time:query", legacy_time_handler);
-module:hook("iq/host/jabber:iq:time:query", legacy_time_handler);
