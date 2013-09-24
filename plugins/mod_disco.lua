b182c0c544e8f4880799a2d38f918a685680bad7
Marco Cirillo <maranda@lightwitch.org>
2013-09-24 15:11:40 +0200

    mod_disco: show only child components by default.

--- plugins/mod_disco.lua
+++ plugins/mod_disco.lua
@@ -13,8 +13,11 @@ local jid_split = require "util.jid".split;
 local jid_bare = require "util.jid".bare;
 local st = require "util.stanza"
 local calculate_hash = require "util.caps".calculate_hash;
+local ipairs, pairs = ipairs, pairs;
+local hosts, metronome_events, my_host = hosts, metronome.events, module.host;
 
-local disco_items = module:get_option("disco_items") or {};
+local show_hosts = module:get_option_boolean("disco_show_hosts", false);
+local disco_items = module:get_option_table("disco_items", {});
 do -- validate disco_items
 	for _, item in ipairs(disco_items) do
 		local err;
@@ -27,18 +30,18 @@ do -- validate disco_items
 		end
 		if err then
 			module:log("error", "option disco_items is malformed: %s", err);
-			disco_items = {}; -- TODO clean up data instead of removing it?
+			disco_items = {};
 			break;
 		end
 	end
 end
 
-module:add_identity("server", "im", module:get_option_string("name", "Metronome")); -- FIXME should be in the non-existing mod_router
+module:add_identity("server", "im", module:get_option_string("name", "Metronome"));
 module:add_feature("http://jabber.org/protocol/disco#info");
 module:add_feature("http://jabber.org/protocol/disco#items");
 
 -- Generate and cache disco result and caps hash
-local _cached_server_disco_info, _cached_server_caps_feature, _cached_server_caps_hash;
+local _cached_server_disco_info, _cached_server_caps_feature, _cached_server_caps_hash, _cached_children_data;
 local function build_server_disco_info()
 	local query = st.stanza("query", { xmlns = "http://jabber.org/protocol/disco#info" });
 	local done = {};
@@ -85,6 +88,16 @@ local function get_server_caps_hash()
 	if not _cached_server_caps_hash then build_server_disco_info(); end
 	return _cached_server_caps_hash;
 end
+local function build_cached_children_data()
+	_cached_children_data = {};
+	if not show_hosts then
+		for jid, name in pairs(get_children(my_host)) do
+			if hosts[jid].type ~= "component" then _cached_children_data[jid] = name; end
+		end
+	else
+		for jid, name in pairs(get_children(my_host)) do _cached_children_data[jid] = name; end
+	end
+end
 
 module:hook("item-added/identity", clear_disco_cache);
 module:hook("item-added/feature", clear_disco_cache);
@@ -111,7 +124,7 @@ module:hook("iq/host/http://jabber.org/protocol/disco#items:query", function(eve
 	if node and node ~= "" then return; end -- TODO fire event?
 
 	local reply = st.reply(stanza):query("http://jabber.org/protocol/disco#items");
-	for jid, name in pairs(get_children(module.host)) do
+	for jid, name in pairs(_cached_children_data) do
 		reply:tag("item", {jid = jid, name = name~=true and name or nil}):up();
 	end
 	for _, item in ipairs(disco_items) do
@@ -133,7 +146,7 @@ module:hook("iq/bare/http://jabber.org/protocol/disco#info:query", function(even
 	if stanza.attr.type ~= "get" then return; end
 	local node = stanza.tags[1].attr.node;
 	local username = jid_split(stanza.attr.to) or origin.username;
-	if not stanza.attr.to or is_contact_subscribed(username, module.host, jid_bare(stanza.attr.from)) then
+	if not stanza.attr.to or is_contact_subscribed(username, my_host, jid_bare(stanza.attr.from)) then
 		local reply = st.reply(stanza):tag("query", {xmlns = "http://jabber.org/protocol/disco#info"});
 		if not reply.attr.from then reply.attr.from = origin.username.."@"..origin.host; end -- COMPAT To satisfy Psi when querying own account
 		module:fire_event("account-disco-info", { origin = origin, stanza = reply, node = node });
@@ -152,10 +165,16 @@ module:hook("iq/bare/http://jabber.org/protocol/disco#items:query", function(eve
 	if stanza.attr.type ~= "get" then return; end
 	local node = stanza.tags[1].attr.node;
 	local username = jid_split(stanza.attr.to) or origin.username;
-	if not stanza.attr.to or is_contact_subscribed(username, module.host, jid_bare(stanza.attr.from)) then
+	if not stanza.attr.to or is_contact_subscribed(username, my_host, jid_bare(stanza.attr.from)) then
 		local reply = st.reply(stanza):tag("query", {xmlns = "http://jabber.org/protocol/disco#items"});
 		if not reply.attr.from then reply.attr.from = origin.username.."@"..origin.host; end -- COMPAT To satisfy Psi when querying own account
 		module:fire_event("account-disco-items", { origin = origin, stanza = reply, node = node });
 		return origin.send(reply);
 	end
+end);
+module:hook_global("host-activated", function(host)
+	if host:match("%.(.*)") == my_host then build_cached_children_data(); end
+end);
+module:hook_global("host-deactivated", function(host)
+	if host:match("%.(.*)") == my_host then build_cached_children_data(); end
 end);
