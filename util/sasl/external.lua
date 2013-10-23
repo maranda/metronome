7626b09ccf89a20fadcb9fcae6b60e1dc379861b
Marco Cirillo <maranda@lightwitch.org>
2013-10-23 14:06:27 +0200

    util.sasl.external: code refactor, - Consistently have the backend perform all sanity check (including checking for user existence). - The processing handler shall handle only the eventual username / boolean and error.

--- util/sasl/external.lua
+++ util/sasl/external.lua
@@ -8,8 +8,8 @@ local log = require "util.logger".init("sasl");
 local jid_compare = require "util.jid".compare;
 local jid_split = require "util.jid".prepped_split;
 local nodeprep = require "util.encodings".stringprep.nodeprep;
-local get_time = os.time;
-local ipairs = ipairs;
+local user_exists = require "core.usermanager".user_exists;
+local get_time, ipairs = os.time, ipairs;
 
 module "sasl.external"
 
@@ -17,10 +17,32 @@ module "sasl.external"
 Supported Authentication Backends
 
 external:
-		function(sasl, session)
-			return cert or nil, err.
+		function(sasl, session, authid)
+			return nil or false or username, err.
 		end
 ]]--
+
+local function external(self, authid)
+	local username, err = self.profile.external(self, self.profile.session, authid);
+
+	if username == nil then
+		log("debug", "A server error was caught while attempting EXTERNAL SASL: %s", err);
+		return "failure", "internal-server-error", err;
+	end
+	if username == false then return "failure", "not-authorized", err; end
+	
+	self.username = nodeprep(username);
+	if self.username then
+		return "success";
+	else
+		log("debug", "Username %s in the certificate, violates the NodePREP profile", username);
+		return "failure", "malformed-request", "Username in the certificate violates the NodePREP profile";
+	end
+end
+
+function init(registerMechanism)
+	registerMechanism("EXTERNAL", {"external"}, external);
+end
 
 local function extract_data(cert)
 	local extensions = cert:extensions();
@@ -40,56 +62,39 @@ local function extract_data(cert)
 	return xmpp_addresses;
 end
 
-local function external(self, authid)
-	local _log = self.profile.session and self.profile.session.log;
-	local cert, err = self.profile.external(self, self.profile.session);
-
-	if err then
-		(_log or log)("debug", "A server error was caught while attempting EXTERNAL SASL: %s", err);
-		return "failure", "internal-server-error", err;
-	end
-	if not cert then
-		(_log or log)("debug", "No certificate available");
-		return "failure", "not-authorized", "No certificate could be obtained";
-	end
-	
-	if not cert:validat(get_time()) then
-		(_log or log)("debug", "The certificate supplied is expired");
-		return "failure", "not-authorized", "Supplied certificate is expired";
-	end
-	
-	local data = extract_data(cert);
-	for _, address in ipairs(data) do
-		if authid == "" or jid_compare(authid, address) then
-			local username, host = jid_split(address);
-			if host == self.host then
-				self.username = nodeprep(username);
-				return "success";
-			end
-		end
-	end
-	
-	return "failure", "not-authorized";
-end
-
-function init(registerMechanism)
-	registerMechanism("EXTERNAL", {"external"}, external);
-end
-
-function backend(sasl, session)
+function backend(sasl, session, authid) -- default exportable backend
 	local socket = session.conn:socket();
 	if not socket.getpeercertificate or not socket.getpeerverification then
 		log("error", "LuaSec 0.5+ is required in order to perform SASL external");
-		return nil, "Unable to perform external certificate authentications at this time.";
+		return nil, "Unable to perform external certificate authentications at this time";
 	end
 	
 	local chain, errors = socket:getpeerverification();
 	if not chain then
 		local _log = session.log or log;
 		_log("warn", "Invalid client certificate chain detected");
-		return nil, "Invalid client certificate chain";
+		return false, "Invalid client certificate chain";
 	end
-	return socket:getpeercertificate();
+
+	local cert = socket:getpeercertificate();
+	if not cert then
+		_log("warn", "Client attempted SASL External without a certificate");
+		return false, "No certificate found";
+	end
+	if not cert:validat(get_time()) then
+		_log("warn", "Client attempted SASL External with an expired certificate");
+		return false, "Supplied certificate is expired";
+	end	
+
+	local data = extract_data(cert);
+	for _, address in ipairs(data) do
+		if authid == "" or jid_compare(authid, address) then
+			local username, host = jid_split(address);
+			if host == self.host and user_exists(username, host) then	return username; end
+		end
+	end
+	
+	return false, "Couldn't find a valid address which could be associated with an xmpp account";
 end
 
 return _M;
