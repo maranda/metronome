3ebb223e90d61e2ab829772c5d24a700fa45e4c1
Marco Cirillo <maranda@lightwitch.org>
2013-12-25 21:43:36 +0100

    mod_lastactivity: do persist user last activity to the datastore. (Fixes #64)

--- plugins/mod_lastactivity.lua
+++ plugins/mod_lastactivity.lua
@@ -11,18 +11,21 @@ local st = require "util.stanza";
 local is_contact_subscribed = require "util.rostermanager".is_contact_subscribed;
 local jid_bare = require "util.jid".bare;
 local jid_section = require "util.jid".section;
+local dm_load = require "util.datamanager".load;
+local dm_store = require "util.datamanager".store;
+local module_host = module.host;
+
+local os_difftime, os_time, tostring = os.difftime, os.time, tostring;
 
 module:add_feature("jabber:iq:last");
 
-local map = {};
-
 module:hook("pre-presence/bare", function(event)
-	local stanza = event.stanza;
+	local username, host, stanza = event.origin.username, event.origin.host, event.stanza;
 	if not(stanza.attr.to) and stanza.attr.type == "unavailable" then
-		local t = os.time();
+		local t = os_time();
 		local s = stanza:child_with_name("status");
 		s = s and #s.tags == 0 and s[1] or "";
-		map[event.origin.username] = {s = s, t = t};
+		dm_store(username, host, "last_activity", { status = s, time = t });
 	end
 end, 10);
 
@@ -30,11 +33,11 @@ module:hook("iq/bare/jabber:iq:last:query", function(event)
 	local origin, stanza = event.origin, event.stanza;
 	if stanza.attr.type == "get" then
 		local username = jid_section(stanza.attr.to, "node") or origin.username;
-		if not stanza.attr.to or is_contact_subscribed(username, module.host, jid_bare(stanza.attr.from)) then
-			local seconds, text = "0", "";
-			if map[username] then
-				seconds = tostring(os.difftime(os.time(), map[username].t));
-				text = map[username].s;
+		if not stanza.attr.to or is_contact_subscribed(username, module_host, jid_bare(stanza.attr.from)) then
+			local seconds, text, data = "0", "", dm_load(username, module_host, "last_activity");
+			if data then
+				seconds = tostring(os_difftime(os_time(), data.time));
+				text = data.status;
 			end
 			origin.send(st.reply(stanza):tag("query", {xmlns = "jabber:iq:last", seconds = seconds}):text(text));
 		else
@@ -43,10 +46,3 @@ module:hook("iq/bare/jabber:iq:last:query", function(event)
 		return true;
 	end
 end);
-
-module.save = function()
-	return {map = map};
-end
-module.restore = function(data)
-	map = data.map or {};
-end
