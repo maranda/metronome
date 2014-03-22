b539c08a4975f30b78adc62512cc0488a11a6c33
Marco Cirillo <maranda@lightwitch.org>
2014-03-22 16:01:41 +0100

    mod_muc_limits: fix arguments order.

--- plugins/mod_muc_limits.lua
+++ plugins/mod_muc_limits.lua
@@ -117,7 +117,7 @@ module:hook("muc-fields-process", function(room, fields, stanza, changed)
 		return st.error_reply(stanza, "cancel", "forbidden", "You need to submit valid number values for muc_limits fields.");
 	end
 	stanzas, seconds = math.max(tonumber(stanzas), 1), math.max(tonumber(seconds), 0);
-	room:set_option(fields[field_enabled], stanzas, changed);
+	room:set_option(field_enabled, fields[field_enabled], changed);
 	room:set_option(field_stanzas, stanzas, changed);
 	room:set_option(field_seconds, seconds, changed);
 end, -101);
