a3d3e59db77c4f29a8d5de71e0db0a664ca802f1
Marco Cirillo <maranda@lightwitch.org>
2014-05-20 15:35:09 +0200

    mod_sic: tostring port value.

--- plugins/mod_sic.lua
+++ plugins/mod_sic.lua
@@ -5,6 +5,7 @@
 -- information about copyright and licensing.
 
 local st_reply = require "util.stanza".reply;
+local tostring = tostring;
 local xmlns = "urn:xmpp:sic:1";
 local attr = { xmlns = xmlns };
 
@@ -16,6 +17,6 @@ module:hook("iq-get/self/"..xmlns..":address", function(event)
 	local port = origin.conn:port();
 	local reply = st_reply(stanza):tag("address", attr);
 	reply:tag("ip"):text(ip):up();
-	if port then reply:tag("port"):text(port):up(); end
+	if port then reply:tag("port"):text(tostring(port)):up(); end
 	return origin.send(reply);
 end);
