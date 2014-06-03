396d5c089ce1fd781a9717172232224a2ffd6666
Marco Cirillo <maranda@lightwitch.org>
2014-06-03 12:31:13 +0200

    mod_dialback: sort handling of destroyed verifying sessions. (Fixes #156)

--- plugins/mod_dialback.lua
+++ plugins/mod_dialback.lua
@@ -200,27 +200,27 @@ module:hook("stanza/"..xmlns_db..":verify", function(event)
 	if origin.type == "s2sout_unauthed" or origin.type == "s2sout" then
 		local attr = stanza.attr;
 		local dialback_verifying = dialback_requests[attr.from.."/"..(attr.id or "")];
-		if dialback_verifying and attr.from == origin.to_host then
+		dialback_requests[attr.from.."/"..(attr.id or "")] = nil;
+		if dialback_verifying and attr.from == origin.to_host and not dialback_verifying.destroyed then
 			local valid, authed;
 			if attr.type == "valid" then
 				authed = make_authenticated(dialback_verifying, attr.from);
 				valid = "valid";
 			elseif attr.type == "error" then
-				dialback_requests[attr.from.."/"..(attr.id or "")] = nil;
 				return handle_db_errors(origin, stanza);
 			else
 				log("warn", "authoritative server for %s denied the key", attr.from or "(unknown)");
 				valid = "invalid";
 			end
-			if authed and dialback_verifying.destroyed then
-				log("warn", "Incoming s2s session %s was closed in the meantime, so we can't notify it of the db result", tostring(dialback_verifying):match("%w+$"));
-			elseif authed then
-				dialback_verifying.sends2s(
-					st.stanza("db:result", { from = attr.to, to = attr.from, id = attr.id, type = valid }):text(dialback_verifying.hosts[attr.from].dialback_key)
-				);
+			dialback_verifying.sends2s(
+				st.stanza("db:result", { from = attr.to, to = attr.from, id = attr.id, type = valid })
+					:text(dialback_verifying.hosts[attr.from].dialback_key));
 			end
-			dialback_requests[attr.from.."/"..(attr.id or "")] = nil;
-			if not authed then origin:close("not-authorized", "authentication failure"); end -- we close the outgoing stream
+			if not authed and not dialback_verifying.multiplexed_from then
+				origin:close("not-authorized", "authentication failure");
+			end
+		else
+			origin:close("not-authorized", "authentication failure");
 		end
 		return true;
 	end
