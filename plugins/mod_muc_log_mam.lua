bfe743a94d2f1c8cae872d3c025aa619b7619c2b
Marco Cirillo <maranda@lightwitch.org>
2017-11-08 01:18:54 +0100

    mod_muc_log_mam: revert change.

--- plugins/mod_muc_log_mam.lua
+++ plugins/mod_muc_log_mam.lua
@@ -137,7 +137,6 @@ module:hook("iq-set/bare/"..xmlns..":query", function(event)
 	
 		for _, message in ipairs(messages) do
 			message.attr.to = origin.full_jid;
-			message.attr.type = "groupchat";
 			origin.send(message);
 		end
 		origin.send(reply);
