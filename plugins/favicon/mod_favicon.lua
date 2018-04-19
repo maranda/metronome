4b5036446c8e773ee73ef854e1418993720d1a0d
Marco Cirillo <maranda@lightwitch.org>
2018-04-19 16:06:36 +0200

    mod_{...}: attempt fixing issues with MS Edge and mod_favicon.

--- plugins/favicon/mod_favicon.lua
+++ plugins/favicon/mod_favicon.lua
@@ -8,22 +8,24 @@ module:set_global()
 
 local server = require "net.http.server"
 
-local favicon = module:get_option_string("favicon_path", (metronome.paths.plugins or "./").."favicon/favicon.ico")
+local favicon_file_path = module:get_option_string("favicon_path", (metronome.paths.plugins or "./").."favicon/favicon.png")
+local favicon_url = module:get_option_string("favicon_url", "/favicon.png")
+local favicon_mime = module:get_option_string("favicon_mime", "image/png")
 local load = require "util.auxiliary".load_file
 
 local function serve_icon(event)
 	local response = event.response
-    	local icon = load(favicon, "rb")
+    local icon = load(favicon_file_path, "rb")
 
 	if not icon then
-		module:log("error","Couldn't find favicon in %s", favicon)
+		module:log("error","Couldn't find favicon in %s", favicon_file_path)
 		return 404
 	else
-		response.headers.content_type = "image/x-icon"
+		response.headers.content_type = favicon_mime
 		return response:send(icon)
 	end
 end
 
 function module.add_host(module)
-	module:hook_object_event(server, "GET "..module.host.."/favicon.ico", serve_icon, -1)
+	module:hook_object_event(server, "GET "..module.host..favicon_url, serve_icon, -1)
 end
