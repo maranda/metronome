350a7a42cb0d9a75e238103e9ff8ac85de85c13a
Marco Cirillo <maranda@lightwitch.org>
2018-04-19 20:15:08 +0200

    mod_favicon: simplify code, fix content type header.
    Also re-add ico version of favicon

--- plugins/favicon/mod_favicon.lua
+++ plugins/favicon/mod_favicon.lua
@@ -8,24 +8,28 @@ module:set_global()
 
 local server = require "net.http.server"
 
-local favicon_file_path = (metronome.paths.plugins or "./").."favicon/favicon.png"
-local favicon_url = "/favicon.png"
-local favicon_mime = "image/png"
+local favicon_file_path = (metronome.paths.plugins or "./").."favicon/favicon."
 local load = require "util.auxiliary".load_file
 
-local function serve_icon(event)
+local function get_icon(event, type)
 	local response = event.response
-    local icon = load(favicon_file_path, "rb")
+    local icon = load(favicon_file_path .. type, "rb")
 
 	if not icon then
-		module:log("error","Couldn't find favicon in %s", favicon_file_path)
 		return 404
 	else
-		response.headers.content_type = favicon_mime
+		if type == "ico" then type = "x-icon" end
+		response.headers["Content-Type"] = "image/" .. type
 		return response:send(icon)
 	end
 end
 
+local function serve(event)
+	local type = event.request.path:match("%.([^%.]*)$")
+	return get_icon(event, type)
+end
+
 function module.add_host(module)
-	module:hook_object_event(server, "GET "..module.host..favicon_url, serve_icon, -1)
+	module:hook_object_event(server, "GET "..module.host.."/favicon.ico", serve, -1)
+	module:hook_object_event(server, "GET "..module.host.."/assets/favicon.png", serve, -1)
 end
