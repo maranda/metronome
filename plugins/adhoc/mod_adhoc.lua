b83da4f49e13c3c0bbc121586dffab3522ffa949
Marco Cirillo <maranda@lightwitch.org>
2018-05-23 23:30:44 +0200

    mod_adhoc: rename variables.

--- plugins/adhoc/mod_adhoc.lua
+++ plugins/adhoc/mod_adhoc.lua
@@ -27,11 +27,11 @@ module:hook("iq/host/"..xmlns_disco.."#info:query", function (event)
 	if stanza.attr.type == "get" and node then
 		if commands[node] then
 			local privileged = is_admin(stanza.attr.from, stanza.attr.to);
-			local is_local_user = section(stanza.attr.from or origin.host, "host") == module.host;
-			local is_server_user = hosts[section(stanza.attr.from or origin.host, "host")];
+			local local_user = section(stanza.attr.from or origin.host, "host") == module.host;
+			local server_user = hosts[section(stanza.attr.from or origin.host, "host")];
 			if (commands[node].permission == "admin" and privileged)
-				or (commands[node].permission == "local_user" and is_local_user)
-				or (commands[node].permission == "server_user" and is_server_user)
+				or (commands[node].permission == "local_user" and local_user)
+				or (commands[node].permission == "server_user" and server_user)
 				or (commands[node].permission == "user") then
 				reply = st.reply(stanza);
 				reply:tag("query", { xmlns = xmlns_disco.."#info",
@@ -62,8 +62,8 @@ module:hook("iq/host/"..xmlns_disco.."#items:query", function (event)
 	local origin, stanza = event.origin, event.stanza;
 	if stanza.attr.type == "get" and stanza.tags[1].attr.node
 	    and stanza.tags[1].attr.node == xmlns_cmd then
-		local is_local_user = section(stanza.attr.from or origin.host, "host") == module.host;
-		local is_server_user = hosts[section(stanza.attr.from or origin.host, "host")];
+		local local_user = section(stanza.attr.from or origin.host, "host") == module.host;
+		local server_user = hosts[section(stanza.attr.from or origin.host, "host")];
 		local admin = is_admin(stanza.attr.from, stanza.attr.to);
 		local global_admin = is_admin(stanza.attr.from);
 		reply = st.reply(stanza);
@@ -74,8 +74,8 @@ module:hook("iq/host/"..xmlns_disco.."#items:query", function (event)
 			command = commands[node];
 			if (command.permission == "admin" and admin)
 				or (command.permission == "global_admin" and global_admin)
-				or (command.permission == "local_user" and is_local_user)
-				or (command.permission == "server_user" and is_server_user)
+				or (command.permission == "local_user" and local_user)
+				or (command.permission == "server_user" and server_user)
 				or (command.permission == "user") then
 				reply:tag("item", { name = command.name,
 				    node = node, jid = module:get_host() });
@@ -92,14 +92,14 @@ module:hook("iq/host/"..xmlns_cmd..":command", function (event)
 	if stanza.attr.type == "set" then
 		local node = stanza.tags[1].attr.node
 		if commands[node] then
-			local is_local_user = section(stanza.attr.from or origin.host, "host") == module.host;
-			local is_server_user = hosts[section(stanza.attr.from or origin.host, "host")];
+			local local_user = section(stanza.attr.from or origin.host, "host") == module.host;
+			local server_user = hosts[section(stanza.attr.from or origin.host, "host")];
 			local admin = is_admin(stanza.attr.from, stanza.attr.to);
 			local global_admin = is_admin(stanza.attr.from);
 			if (commands[node].permission == "admin" and not admin)
 				or (commands[node].permission == "global_admin" and not global_admin)
-				or (commands[node].permission == "local_user" and not is_local_user)
-				or (commands[node].permission == "server_user" and not is_server_user) then
+				or (commands[node].permission == "local_user" and not local_user)
+				or (commands[node].permission == "server_user" and not server_user) then
 				origin.send(st.error_reply(stanza, "auth", "forbidden", "You don't have permission to execute this command"):up()
 				    :add_child(commands[node]:cmdtag("canceled")
 					:tag("note", {type="error"}):text("You don't have permission to execute this command")));
