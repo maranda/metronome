af72038a2fbe7d306445f7b13edbf6e6939f5e13
Marco Cirillo <maranda@lightwitch.org>
2018-07-20 16:50:30 +0200

    mod_register_api: fix previous commit and re-release.

--- plugins/register_api/mod_register_api.lua
+++ plugins/register_api/mod_register_api.lua
@@ -511,8 +511,8 @@ end
 
 local function handle_user_registration(event)
 	local user, hostname, password, data, session = event.username, event.host, event.password, event.data, event.session;
-	local mail = event.source == "mod_register" and data.email and data.email:lower();
-	if do_mail_verification then
+	if do_mail_verification and event.source == "mod_register" then
+		local mail = data.email and data.email:lower();
 		if not mail or not hashes:add(user, mail) then
 			module:log("warn", "%s register form doesn't have mail data or failed to add the address hash (mail provided is: %s)", 
 				user, tostring(mail));
