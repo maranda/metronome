213eb3d5831548750ef1f83ff022c2c1153bba26
Marco Cirillo <maranda@lightwitch.org>
2018-08-26 03:02:56 +0200

    mam.lib: rollback returning item-not-found when next item is out bounds.

--- plugins/mam/mam.lib.lua
+++ plugins/mam/mam.lib.lua
@@ -279,7 +279,9 @@ local function generate_stanzas(store, start, fin, with, max, after, before, ind
 			-- we clone the table upto index
 			for i = (sub < 0 and 1) or sub, entry_index do to_process[#to_process + 1] = logs[i]; end
 		end
-		if #to_process == 0 then return nil; end
+		if #to_process == 0 then
+			return stanzas, generate_fin(stanzas, first, last, entries_count, count, true), #stanzas;
+		end
 
 		for i, entry in ipairs(to_process) do
 			local timestamp = entry.timestamp;
@@ -299,7 +301,9 @@ local function generate_stanzas(store, start, fin, with, max, after, before, ind
 		to_process = {};
 		-- we clone table from index
 		for i = entry_index, #logs do to_process[#to_process + 1] = logs[i]; end
-		if #to_process == 0 then return nil; end
+	end
+	if #to_process == 0 then
+		return stanzas, generate_fin(stanzas, first, last, entries_count, count, true), #stanzas;
 	end
 	
 	for i, entry in ipairs(to_process or logs) do
