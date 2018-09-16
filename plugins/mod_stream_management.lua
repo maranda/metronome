0ae9ef678ae52bfd4c54393c8b1aebf70737f33a
Marco Cirillo <maranda@lightwitch.org>
2018-09-16 20:47:45 +0200

    mod_stream_management: correct #430 commit.

--- plugins/mod_stream_management.lua
+++ plugins/mod_stream_management.lua
@@ -136,9 +136,9 @@ local function wrap(session, _r, xmlns_sm) -- SM session wrapper
 			end
 			for _, queued in ipairs(_q) do
 				local name = queued.name;
-				if (name == "iq" or name == "message" or name == "presence") and queued.attr.to and queued.attr.type ~= "error" then
+				if (name == "iq" or name == "message" or name == "presence") and queued.attr.type ~= "error" then
 					local reply = st_reply(queued);
-					if reply.attr.to ~= full_jid and (has_carbons and name ~= "message" or not has_carbons) then
+					if reply.attr.to and reply.attr.to ~= full_jid and (has_carbons and name ~= "message" or not has_carbons) then
 						reply.attr.type = "error";
 						reply:tag("error", { type = "cancel" }):tag("recipient-unavailable", { xmlns = xmlns_e });
 						fire_event("route/process", session, reply);
