25ad87472f0b8bc9d1f0e54e741aeb30e7d1260f
Marco Cirillo <maranda@lightwitch.org>
2019-11-21 08:43:30 +0100

    share.php: fix indenting whitespaces.

--- plugins/http_upload_external/resources/share.php
+++ plugins/http_upload_external/resources/share.php
@@ -9,8 +9,8 @@
 
   In Metronome set:
 
-    http_file_external_url = "https://your.example.com/path/to/share.php/"
-    http_file_secret = "this is your secret string"
+	http_file_external_url = "https://your.example.com/path/to/share.php/"
+	http_file_secret = "this is your secret string"
 	http_file_delete_secret = "this is your secret string"
 
   ** Metronome's License
