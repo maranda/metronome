35eaee88cb5d81d3a680eccd8256ea3444bf7556
Marco Cirillo <marco.cirillo@aria-net.org>
2022-07-24 21:50:39 +0200

    {...}: Implement shared stanza logging facilities.
    Mainly for groupchats use:
    - Implement a module holding the shared archive and event hooks
    - Implement utility library

---
+++ plugins/auxlibs/stanzalog.lib.lua
@@ -0,0 +1,126 @@
+-- * Metronome IM *
+--
+-- This file is part of the Metronome XMPP server and is released under the
+-- ISC License, please see the LICENSE file in this source package for more
+-- information about copyright and licensing.
+
+local uuid = require "util.uuid".generate;
+local get_actions = module:require("acdf", "auxlibs").get_actions;
+local deserialize = require "util.stanza".deserialize;
+
+local os_date, t_insert = os.date, table.insert;
+
+local store_elements = module:get_option_set("stanza_log_allowed_elements", {});
+store_elements:add("acknowledged");
+store_elements:add("apply-to");
+store_elements:add("displayed");
+store_elements:add("encrypted");
+store_elements:add("encryption");
+store_elements:add("markable");
+store_elements:add("mix");
+store_elements:add("openpgp");
+store_elements:add("securitylabel");
+store_elements:add("received");
+store_elements:remove("body");
+store_elements:remove("html");
+store_elements:remove("origin-id");
+store_elements:remove("replace");
+
+local function process_stanza(source, stanza, data)
+	local oid = stanza:get_child("origin-id", sid_xmlns);
+	local body = stanza:child_with_name("body");
+	local subject = stanza:child_with_name("subject");
+	local id = stanza.attr.id;
+	local uid = uuid();
+
+	local data_entry = {
+		time = os_date("!%X"),
+		timestamp = os.time(),
+		from = source,
+		resource = stanza.attr.from,
+		id = stanza.attr.id,
+		oid = oid and oid.attr.id, -- needed for mod_muc_log_mam
+		uid = uid,
+		type = stanza.attr.type, -- needed for mod_muc_log_mam
+		body = body and body:get_text(),
+		subject = subject and subject:get_text()
+	};
+	data[#data + 1] = data_entry;
+
+	-- store elements
+
+	local tags = {};
+	local elements = stanza.tags;
+	for i = 1, #elements do
+		local element = elements[i];
+		if store_elements:contains(element.name) or (element.name == "html" and html) then
+			if element.name == "securitylabel" and element.attr.xmlns == labels_xmlns then
+				local text = element:get_child_text("displaymarking");
+				data_entry.label_actions = get_actions(mod_host, text);
+				data_entry.label_name = text;
+			end
+			t_insert(tags, deserialize(element));
+		end
+	end
+	if not next(tags) then
+		tags = nil;
+	else
+		data_entry.tags = tags;
+	end
+
+	return data, data_entry;
+end
+
+function get_days_in_month(m, y)
+  return os_date('*t', os.time{year=y, month=m+1, day=0})['day'];
+end
+
+function calculate_set(start, fin, before, after)
+	local start_m, start_yr = os_date("!%m", start), os_date("!%Y", start);
+	local end_m, end_yr = os_date("!%m", fin), os_date("!%Y", fin);
+	local year_offset = tonumber(end_yr) - tonumber(start_yr);
+	local month_offset = tonumber(end_m) - tonumber(start_m);
+	if year_offset < 0 or month_offset < 0 then
+		return false; -- not supporting flipped results
+	end
+	local start_date, end_date = os_date("!%Y%m%d", start), os_date("!%Y%m%d", fin)
+	local set = {}
+	-- add dates
+	local month, day, year, end_d = start_m, os_date("!%d", start), start_yr, os_date("!%d", fin);
+	local remaining_days;
+	repeat
+		remaining_days = get_days_in_month(tonumber(month), tonumber(year)) - tonumber(day);
+		set[#set + 1] = tostring(year) .. tostring(month) .. tostring(day);
+		day = tonumber(day) + 1;
+		if day > get_days_in_month(tonumber(month), tonumber(year)) then day = 1 end
+		if day < 10 then day = "0" .. tostring(day); else day = tostring(day); end
+		if remaining_days <= 0 then -- switch month
+			month = tonumber(month) + 1;
+			if month > 12 then month = 1; year = tostring(tonumber(year) + 1); end
+			if month < 10 then month = "0" .. tostring(month); else month = tostring(month); end
+		end
+	until set[#set] >= end_date
+	return set;
+end
+
+local function load_batch(node, host, archive, start, fin, before, after, metadata)
+	local set = calculate_set(start, fin, before, after);
+	for k, value in ipairs(set) do
+		local data = storagemanager.open(host, "stanza_log" .. "/" .. value):get(node);
+		if data then
+			for n, entry in ipairs(data) do	archive[#archive + 1] = entry; end
+		end
+	end
+	t_sort(archive, function(a, b) return a.timestamp < b.timestamp; end);
+	if #archive > 0 and (not metadata.start or metadata.start > archive[1].timestamp) then
+		metadata.start = archive[1].timestamp;
+	end
+	if #archive > 0 and (not metadata.last or metadata.last > archive[#archive].timestamp) then
+		metadata.last = archive[#archive].timestamp;
+	return archive;
+end
+
+return {
+	process_stanza = process_stanza;
+	load_batch = load_batch;
+}
---
+++ plugins/mod_stanza_log.lua
@@ -0,0 +1,64 @@
+-- * Metronome IM *
+--
+-- This file is part of the Metronome XMPP server and is released under the
+-- ISC License, please see the LICENSE file in this source package for more
+-- information about copyright and licensing.
+
+local util_jid = require "util.jid";
+local storagemanager = require "core.storagemanager";
+
+local os_date = os.date;
+
+local stanzalog_lib = module:require("stanzalog", "auxlibs")
+local logs = module:shared("archives");
+
+local function load_stanza_log(node, host, start, fin, before, after)
+	local jid = util_jid.join(node, host)
+	local metadata = storagemanager.open(host, "stanza_log_metada"):get(node) or {};
+	local log, set = logs[util_jid.join(node, host)];
+	if not log then
+		logs[jid] = stanzalog_lib.load_batch(node, host, logs[jid] or {}, fin, before, after, metadata);
+		storagemanager.open(host, "stanza_log_metada"):store(node, metadata);
+		return logs[jid];
+	end
+	if not before and not after and (metadata.start and start > metadata.start) and (metadata.last and last < metadata.last) then
+		return log;
+	else
+		if before then -- one month before
+			start = start - 2630000;
+		elseif after then -- one month after
+			after = after + 2630000;
+		end
+	end
+	log = stanzalog_lib.load_batch(node, host, log, fin, before, after, metadata);
+	storagemanager.open(host, "stanza_log_metada"):store(node, metadata);
+	return log;
+end
+
+local function store_stanza_log(node, host, data) {
+	local metadata = storagemanager.open(host, "stanza_log_metada"):get(node) or {};
+	metadata.last = data.timestamp;
+	local jid = util_jid.join(node, host);
+	if logs[jid] then
+		logs[jid] = data;
+	end
+	storagemanager.open(host, "stanza_log_metada"):store(node, metadata);
+	return storagemanager.open(host, "stanza_log/" .. os_date("!%Y%m%d")):store(node, data);
+}
+
+local function purge_stanza_log(node, host) {
+	for store in storagemanager.get_driver(host):stores(node, "keyval", "stanza_log") do
+		storagemanager.open(host, store):store(node, nil);
+	end
+	storagemanager.open(host, "stanza_log_metadata"):store(node, nil);
+}
+
+module:hook("stanza-log-load", load_stanza_log);
+module:hook("stanza-log-store", store_stanza_log);
+module:hook("stanza-log-purge", purge_stanza_log);
+
+return {
+	load_stanza_log = load_stanza_log;
+	store_stanza_log = store_stanza_log;
+	process_stanza = process_stanza;
+}
